49985e104bfee6c3a168e1a7792cb972e1c0b42b
Meiguins <kauanmeiguins3@gmail.com>
2024-02-28 19:30:51 -0300

    adicionando metodo POST, adicionando a pagina cadastro e a pagina lista

--- src/main/java/dev/meiguins/sistemavendas/controller/EstadoController.java
+++ src/main/java/dev/meiguins/sistemavendas/controller/EstadoController.java
@@ -1,8 +1,43 @@
 package dev.meiguins.sistemavendas.controller;
 
+import org.hibernate.boot.jaxb.spi.Binding;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+
+import dev.meiguins.sistemavendas.model.Estado;
+import dev.meiguins.sistemavendas.repository.EstadoRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @Controller
 public class EstadoController {
-    
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    @GetMapping("/cadastroEstados")
+    public ModelAndView cadastrar(Estado estado) {
+        ModelAndView mv = new ModelAndView("administrativo/estados/cadastro");
+        mv.addObject("estado", estado);
+        return mv;
+    }
+
+    @PostMapping("/salvarEstado")
+    public ModelAndView salvar(Estado estado, BindingResult result) {
+        if (result.hasErrors()) {
+            return cadastrar(estado);
+        }
+        estadoRepository.saveAndFlush(estado);
+        return cadastrar(new Estado());
+    }
 }
+    
+     
+    
+    
+
